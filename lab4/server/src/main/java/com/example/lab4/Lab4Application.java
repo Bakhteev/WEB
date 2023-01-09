dd05732b6cf07daf6288a9759489300f7c4f703d
Bakhteev <bakhteevb@gmail.com>
2023-01-09 03:01:49 +0300

    idea fix

---
+++ lab4/server/src/main/java/com/example/lab4/Lab4Application.java
@@ -0,0 +1,12 @@
+package com.example.lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4Application.class, args);
+	}
+}
